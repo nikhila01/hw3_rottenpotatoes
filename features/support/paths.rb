1ae6a9196a1ac9b113f488e6ef5fc7b5244954c9
Nikhil Arora <nikhila01@gmail.com>
2012-03-18 22:38:21 -0700

    Completed part 1. Filled in the step definition for given the following movies exist, and add the RottenPotatoes home page to the paths.rb.

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -16,6 +16,9 @@ module NavigationHelpers
     when /^the home\s?page$/
       '/'
 
+    when /^the RottenPotatoes home page$/
+      '/movies'
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
